ed1211e04cf8a5fc2be14c39abcd9e501fb8359b
diponegoroska <diponegoroska@gmail.com>
2023-08-31 07:54:18 +0700

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -3,7 +3,7 @@
 
 date_default_timezone_set("Asia/Jakarta");
 // database settings
-define("DB_HOST","kelompok3nih.tk");
-define("DB_USERNAME","kelompok3");
-define("DB_USER_PASSWORD","passKelompok3");
+define("DB_HOST","localhost");
+define("DB_USERNAME","root");
+define("DB_USER_PASSWORD","");
 define("DB_NAME","db_ujianonline");
